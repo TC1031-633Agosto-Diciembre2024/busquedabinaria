f0cab39c82785f42e5b2e1b99abe9f35b78b874d
Andrés Fernando Torres Morán <atorres@applab.mx>
2024-08-20 10:20:11 -0600

    Personas

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+
+#include "Persona.h"
 using namespace std;
 
 int* busquedaBinaria(int* arr, int size, int valor) {
@@ -17,6 +19,22 @@ int* busquedaBinaria(int* arr, int size, int valor) {
     return nullptr;
 }
 
+Persona* busquedaBinariaPersonas(Persona* arr,int size,string buscar) {
+    Persona* inicio=arr;
+    Persona* fin= arr + size -1;
+    Persona aBuscar(buscar);
+    while(inicio <= fin) {
+        Persona* medio= inicio + (fin-inicio)/2;
+        if((medio)->getName()==buscar) {
+            return medio;
+        }else if((medio)->getName()<buscar) {
+            inicio=medio+1;
+        }else {
+            fin=medio-1;
+        }
+    }
+    return nullptr;
+}
 
 int main() {
     int arr[]={2,3,5,7,8,10,13,15,27};
@@ -26,6 +44,16 @@ int main() {
     int* resultado=busquedaBinaria(arr,9,buscar);
     if(resultado!=nullptr) {
         cout<<"El elemento "<<buscar<<" está en la pos: "<<resultado-arr<<endl;
+    }else {
+        cout<<"No encontré el elemento"<<endl;
+    }
+    Persona personasArr[]={Persona("Andres"),Persona("Bruno"),Persona("Luis"),Persona("Miguel")};
+    cout<<"Dame el nombre a buscar"<<endl;
+    string personaBuscar;
+    cin>>personaBuscar;
+    Persona* resultadoPersona=busquedaBinariaPersonas(personasArr,4,personaBuscar);
+    if(resultadoPersona!=nullptr) {
+        cout<<"El elemento "<<personaBuscar<<" está en la pos: "<<resultadoPersona-personasArr<<endl;
     }else {
         cout<<"No encontré el elemento"<<endl;
     }
